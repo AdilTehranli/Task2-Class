b7b04c4b2404d94e4d75bb044c33cba85e25e981
AdilTehranli <adiljt@code.edu.az>
2023-04-24 19:38:31 +0400

    Create student class

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Drawing;
+using System.Xml;
+using System.Xml.Linq;
+using task1;
+
+student stu1 = new student("Elsen", "Nesirov", "p137", 80, true);
+
+Console.WriteLine(stu1.FullName());
+Console.WriteLine(stu1.Group());
+stu1.GetPoint();
+stu1.Isgraduated();
---
+++ task2/student.cs
@@ -0,0 +1,62 @@
+﻿
+namespace task1;
+
+internal class student
+{
+    //fields
+    public string name;
+    public string surname;
+    public double point;
+    public string group;
+    public bool isgraduated;
+    public student(string Name, string Surname, string Group, double Point, bool Isgraduated)
+    {
+        name = Name;
+        surname = Surname;
+        group = Group;
+        point = Point;
+        isgraduated = Isgraduated;
+    }
+    public string FullName()
+    {
+        return $"{name} {surname}";
+    }
+    public string Group()
+    {
+        return $"{group}";
+    }
+    public bool Isgraduated()
+    {
+        if (isgraduated)
+        {
+            Console.WriteLine("Mezun oldunuz");
+            return true;
+
+        }
+        else
+        {
+            Console.WriteLine("Mezun olmadiniz");
+            return false;
+
+
+        }
+
+    }
+    public bool GetPoint()
+    {
+        if (point >= 80)
+        {
+            Console.WriteLine("topladiginiz bal kecerlidir");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("bextinizi bir daha sinayin");
+            return false;
+        }
+    }
+
+
+
+
+}
